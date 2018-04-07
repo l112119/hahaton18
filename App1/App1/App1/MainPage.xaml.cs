bf6eb5ae02ea8af9ca32cfa9362640631b5f9d16
vivom <vivomcom@gmail.com>
2018-04-07 22:10:58 +0300

    ntcn

--- App1/App1/App1/MainPage.xaml.cs
+++ App1/App1/App1/MainPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ExifLib;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +11,47 @@ using Xamarin.Forms;
 
 namespace App1
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            StackLayout stackLayout = new StackLayout();
+
+            Button getPhotoBtn = new Button { Text = "Выбрать фото" };
+
+            stackLayout.Children.Add(getPhotoBtn);
+            this.Content = stackLayout;
+
+            Image img = new Image();
+
+            // выбор фото
+            getPhotoBtn.Clicked += async (o, e) =>
+            {
+                //if (CrossMedia.Current.IsPickPhotoSupported)
+                //{
+                //    MediaFile photo = await CrossMedia.Current.PickPhotoAsync();
+                //    img.Source = ImageSource.FromFile(photo.Path);
+                //}
+               // bool b = await CrossMedia.Current.Initialize();
+
+                if (CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    MediaFile photoPicked = await CrossMedia.Current.PickPhotoAsync();
+
+                    if (photoPicked != null)
+                    {
+                        await DisplayAlert("Photo Location", photoPicked.Path, "OK");
+
+                        using (Stream streamPic = photoPicked.GetStream())
+                        {
+                            var picInfo = ExifReader.ReadJpeg(streamPic);
+                            ExifOrientation orientation = picInfo.Orientation;
+                        }
+                    }
+                }
+            };
+        }
+    }
 }
